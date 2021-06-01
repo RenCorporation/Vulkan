135656c1108b47f666ac7085346bbf731d2e4bd2
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2021-06-01 18:13:26 +0530

    Added framebuffers to Source/Vulkan/Vulkan.h

--- Source/Vulkan/Framebuffers/Framebuffers.hpp
+++ Source/Vulkan/Framebuffers/Framebuffers.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
-
+namespace Vulkan::Framebuffers {
+    
+}
--- Source/Vulkan/Vulkan.hpp
+++ Source/Vulkan/Vulkan.hpp
@@ -2,11 +2,12 @@
 
 #include "Device/Device.hpp"
 #include "Extensions/Extensions.hpp"
-#include "RenderPass/RenderPass.hpp"
+#include "Framebuffers/Framebuffers.hpp"
 #include "GraphicsPipeline/GraphicsPipeline.hpp"
 #include "ImageView/ImageView.hpp"
 #include "Instance/Instance.hpp"
 #include "Queue/Queue.hpp"
+#include "RenderPass/RenderPass.hpp"
 #include "Surface/Surface.hpp"
 #include "SwapChain/SwapChain.hpp"
 #include "ValidationLayer/ValidationLayer.hpp"
